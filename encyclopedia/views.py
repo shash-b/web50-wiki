d2d9214584150e8463c7eaf9187aa3cc2b146cca
Shashwat <shashwat.bhandari.14@gmail.com>
2023-05-30 22:57:36 +0800

    Complete first step

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,9 +2,24 @@ from django.shortcuts import render
 
 from . import util
 
+from markdown2 import Markdown
+markdowner = Markdown()
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    entry = util.get_entry(title)
+    content, title = "", "Error 404"
+
+    if entry:
+        content = markdowner.convert(entry)
+        title = content.split("<h1>")[1].split("</h1>")[0]
+    
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "entry": entry,
+        "content": content
+    })
