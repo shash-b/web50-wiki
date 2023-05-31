505cedb6e20f150f04d71a0038221951442b4e83
Shashwat <shashwat.bhandari.14@gmail.com>
2023-05-31 15:07:39 +0800

    Update search

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,31 +1,28 @@
 from django.shortcuts import render
-
+from django import forms
 from . import util
-
 from markdown2 import Markdown
+
 markdowner = Markdown()
+
+class NewEntryForm(forms.Form):
+    title = forms.CharField(label="New title")
+    markdown = forms.CharField(widget=forms.Textarea)
 
 def index(request):
     if request.method == "POST":
-        query = request.POST['q']
-
-        if query in [i.lower() for i in util.list_entries()]:
-            return entry(request, query)
-        
-        entries = []
-        for item in util.list_entries():
-            if request.POST['q'] in item.lower():
-                entries.append(item)
-
-        return render(request, "encyclopedia/search.html", {
-            "entries": entries
-        })
+        if request.POST['q']:
+            return search(request, False)
 
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
 def entry(request, title):
+    if request.method == "POST":
+        if request.POST['q']:
+            return search(request, True)
+
     entry = util.get_entry(title)
     content, title = "", "Error 404"
 
@@ -37,4 +34,44 @@ def entry(request, title):
         "title": title,
         "entry": entry,
         "content": content
+    })
+
+
+def new(request):
+    if request.method == "POST":
+        if request.POST['q']:
+            return search(request, False)
+
+    return render(request, "encyclopedia/new.html", {
+        "form": NewEntryForm()
+    })
+
+
+def search(request, is_entry):
+    query = request.POST['q']
+    full_query = query in [i.lower() for i in util.list_entries()]
+
+    if full_query and not is_entry:
+        return entry(request, query)
+    
+    elif full_query and is_entry:
+        data = util.get_entry(query)
+        content = markdowner.convert(data)
+        title = content.split("<h1>")[1].split("</h1>")[0]
+    
+        return render(request, "encyclopedia/entry.html", {
+            "entry": data,
+            "title": title,
+            "content": content
+        })
+    
+    entries = []
+    for item in util.list_entries():
+        if request.POST['q'] in item.lower():
+            entries.append(item)
+
+    print(entries)
+    return render(request, "encyclopedia/search.html", {
+        "entries": entries,
+        "from_entry": is_entry
     })
